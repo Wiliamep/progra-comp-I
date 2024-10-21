f4348a6f454c9020aa786b1f98854e8d84019591
William Torres <ISNP111124@ugb.edu.sv>
2024-10-20 22:23:22 -0600

    Suma de dos numeros introducidos por el usuario

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,7 +10,17 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola teacher de Programacion Computacional");
+            //suma de dos numeros, introducidos por el usuario
+            Console.Write("Num 1: ");
+            int num1 = Int16.Parse(Console.ReadLine()); // "5" -> 5
+
+            Console.Write("Num 2: ");
+            int num2 = Int16.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+            Console.WriteLine("La suma es: {0}", suma);
+
+
             Console.WriteLine(); //para que no se cierre la consola
         }
     }
